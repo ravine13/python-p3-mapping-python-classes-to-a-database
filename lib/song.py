1ff1fa98d390a2dd71208d3cfc2d963b73568cd8
ravine13 <kuriaderek@gmail.com>
2023-12-04 15:41:49 +0300

    passed test

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,39 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        CURSOR.execute(sql)
+
+    @classmethod
+    def create(cls, name, album):
+        sql = """
+            INSERT INTO songs(name, album)
+            VALUES(?, ?)
+        """
+        CURSOR.execute(sql, (name, album))
+        CONN.commit()
+        song = cls(name, album)
+        song.id = CURSOR.lastrowid
+        return song
+    
+    def save(self):
+        sql = """
+            INSERT INTO songs(name, album)
+            VALUES(?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
